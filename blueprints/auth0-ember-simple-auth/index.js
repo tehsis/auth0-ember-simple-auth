4932fa27c14073c972507a7a640bbe4eda2fe32f
Aram Zadikian <brancusi@gmail.com>
2015-07-28 11:05:08 -0700

    Updating deps

--- blueprints/auth0-ember-simple-auth/index.js
+++ blueprints/auth0-ember-simple-auth/index.js
@@ -4,9 +4,9 @@ module.exports = {
 
   afterInstall: function() {
     return this.addBowerPackagesToProject([
-      { name: 'ember-simple-auth',      target: '0.8.0-beta.3'  },
-      { name: 'auth0-lock',             target: '~7.5.6'  },
-      { name: 'jsrsasign',              target: '~4.8.2'  }
+      { name: 'ember-simple-auth',      target: '0.8.0'  },
+      { name: 'auth0-lock',             target: '~7.7.1'  },
+      { name: 'jsrsasign',              target: '~4.8.3'  }
     ]);
   }
 };
