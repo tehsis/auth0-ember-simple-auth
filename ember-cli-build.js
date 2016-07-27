4f54fece34658989221b819734fd27db3a669db3
Pablo Terradillos <tehsis@gmail.com>
2016-07-27 02:18:28 -0300

    Fixed lock path

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -12,7 +12,7 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-  app.import(app.bowerDirectory + '/auth0-lock/build/auth0-lock.js');
+  app.import(app.bowerDirectory + '/auth0-lock/build/lock.js');
   app.import(app.bowerDirectory + '/jsrsasign/jsrsasign-latest-all-min.js');
 
   return app.toTree();
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'auth0-ember-simple-auth',
 
   included: function(app) {
-    app.import(app.bowerDirectory + '/auth0-lock/build/auth0-lock.js');
+    app.import(app.bowerDirectory + '/auth0-lock/build/lock.js');
     app.import(app.bowerDirectory + '/jsrsasign/jsrsasign-latest-all-min.js');
   }
 };
